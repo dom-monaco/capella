a8d85f382ce16b0bc42d90bba8886c4d1b553d6a
dominic monaco <dominicmonaco@MacBook-Pro.localdomain>
2023-11-16 10:38:34 -0500

    add encode endpoint logic and test

--- main.py
+++ main.py
@@ -3,13 +3,27 @@ import hashlib
 
 app = Flask(__name__)
 
+# Dictionary to store short and original URLs
+url_mapping = {}
+
 @app.route('/encode', methods=['POST'])
 def encode():
+    data = request.get_json()
+
+    original_url = data['url']
+    # Create a unique hash for the original URL to be used as the short version
+    hash_object = hashlib.md5(original_url.encode())
+    short_url = hash_object.hexdigest()[:8]
+
+    # Store the mapping
+    url_mapping[short_url] = original_url
+
+    return jsonify({'short_url': f'http://short/{short_url}'})
 
 
-@app.route('/decode/<short_url>', methods=['GET'])
-def decode(short_url):
-
+# @app.route('/decode/<short_url>', methods=['GET'])
+# def decode(short_url):
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test_url.py
+++ test_url.py
@@ -5,7 +5,12 @@ class UrlShortenerTests(unittest.TestCase):
     base_url = 'http://127.0.0.1:5000'
 
     def test_encode_url(self):
-        return True
+        url_to_shorten = 'http://www.example.com/thisisalongexample'
+        response = requests.post(f'{self.base_url}/encode', json={'url': url_to_shorten})
+        data = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('short_url', data)
 
     def test_decode_url(self):
         return True
@@ -18,4 +23,3 @@ class UrlShortenerTests(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-    
